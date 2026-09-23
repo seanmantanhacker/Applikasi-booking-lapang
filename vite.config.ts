a62b7c3aa7c43dc49f418fa3d43e04fa152113c2
Sean Pribs <pribadi.sean@gmail.com>
2026-09-23 12:11:24 +0900

    ref

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Applikasi-booking-lapang/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
